76a949220f08aec78b6a865099ab45a81913e272
Makaruos-Emad <makaruosemad235@gmail.com>
2024-08-05 23:40:44 +0300

    add register text to login view & create register view

--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -1,9 +1,12 @@
 import 'package:chat_app/features/login/presentation/view/login_view.dart';
+import 'package:chat_app/features/login/presentation/view/register_view.dart';
 import 'package:chat_app/features/splash/presentation/view/splash_view.dart';
 import 'package:go_router/go_router.dart';
 
 abstract class AppRouter {
   static const kLoginView = '/LoginView';
+  static const kRegisterView = '/RegisterView';
+
   static final router = GoRouter(
     routes: [
       GoRoute(
@@ -13,6 +16,10 @@ abstract class AppRouter {
       GoRoute(
         path: kLoginView,
         builder: (context, state) => const LoginView(),
+      ),
+      GoRoute(
+        path: kRegisterView,
+        builder: (context, state) => const RegisterView(),
       )
     ],
   );
---
+++ lib/features/login/presentation/view/widget/register_text.dart
@@ -0,0 +1,37 @@
+import 'package:chat_app/core/utils/app_router.dart';
+import 'package:chat_app/core/utils/app_style_text.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class RegisterText extends StatelessWidget {
+  const RegisterText({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const Text(
+          'don\'t have account ? ',
+          style: StyleText.style24,
+        ),
+        GestureDetector(
+          onTap: () {
+            Future.delayed(
+              const Duration(seconds: 2),
+              () {
+                GoRouter.of(context).push(AppRouter.kRegisterView);
+              },
+            );
+          },
+          child: Text(
+            'Go Register',
+            style: StyleText.style24.copyWith(
+              color: const Color(0xff00ff00),
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
