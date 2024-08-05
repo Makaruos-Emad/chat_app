056d79661db360afd1272380bbe31ce912f6a6f5
Makaruos-Emad <makaruosemad235@gmail.com>
2024-08-05 15:11:02 +0300

    create splash view

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:chat_app/core/utils/app_router.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,6 +10,8 @@ class ChatApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp.router(
+      routerConfig: AppRouter.router,
+    );
   }
 }
